e295dbe9c4282c2aa4aa6b5291dac9ec4309d33c
tada <tada20001@gmail.com>
2018-12-17 17:44:24 +0900

    회원가입과 동시에 로그인하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.views.generic import CreateView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from .forms import SignupForm
 
 # FBV Example
@@ -12,16 +13,25 @@ from .forms import SignupForm
 #         form = UserCreationForm(request.POST)
 #         if form.is_valid():
 #             user = form.save()
-#             return redirect(settings.LOGIN_URL)
+#             #  로그인 처리
+#             auth_login(request, user)
+#             return redirect('profile')
 #     else:
 #         form = UserCreationForm()
 #     return render(request, 'accounts/signup.html', {'form': form, })
 
-signup = CreateView.as_view(
-            template_name='accounts/signup.html',
-            model=User,
-            form_class=SignupForm,
-            success_url=settings.LOGIN_URL)
+class SignupView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'accounts/signup.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect('profile')
+
+signup = SignupView.as_view()
+
 
 @login_required # settings.LOGIN_URL
 def profile(request):
