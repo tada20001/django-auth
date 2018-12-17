423b0b99de61cd843a14e1b572dda881d369bcc8
tada <tada20001@gmail.com>
2018-12-17 14:20:07 +0900

    프로필 페이지 구현하기

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+@login_required # settings.LOGIN_URL
+def profile(request):
+    return render(request, 'accounts/profile.html')
--- auth/urls.py
+++ auth/urls.py
@@ -5,6 +5,6 @@ from django.shortcuts import redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', )),
-    path('', lambda request: redirect('blog:post_list')),
+    path('', lambda request: redirect('blog:post_list'), name='root'),
     path('accounts/', include('accounts.urls')),
 ]
